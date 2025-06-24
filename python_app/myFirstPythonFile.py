e53d9520903d4c69f14ec8502e51675770422a14
Mikeconnor1 <mikeconnor@hotmail.co.uk>
2025-06-24 13:53:09 +0100

    Corrected sayHello function

--- python_app/myFirstPythonFile.py
+++ python_app/myFirstPythonFile.py
@@ -3,6 +3,6 @@
 def sayHello():
     print('Hello, I am a python app')
 
-    if __name__ == '__main__':
-        sayHello()
-        print('I am finished . . .')
+if __name__ == '__main__':
+    sayHello()
+    print('I am finished . . .')
